82afbb6401ccf4433e9284ec5db236b475e98ffe
DabTheMatt <inspiromaciek@gmail.com>
2022-12-27 16:14:46 +0100

    one page start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+
+import OnePage from './OnePage/OnePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Matt develop branch
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>balance_3</h1>
+     <OnePage />
     </div>
   );
 }
